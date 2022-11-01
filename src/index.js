aeb7e75caeb3bd690342158f2b7bee639ca6861b
Eliseo Espindola <eliseoespindola1@gmail.com>
2022-11-01 19:22:45 -0300

    agregamos los middlewares morgan y static

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
 
 //crear una instancia de express
 const app = express();
@@ -6,7 +9,23 @@ const app = express();
 app.set('port', process.env.PORT ||'4000')
 
 app.listen( app.get('port'), ()=>{
-    console.log('Estpy en el puerto ' +app.get('port'))
+    console.log('Estoy en el puerto ' +app.get('port'));
 })
 
-console.log('Hola Mundo')
+//midelwares: funciones que se ejecutan antes de las rutas
+app.use(cors()); //permite conexiones remotas
+//permiten interpretar el formato json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'));
+//cargar un archivo estatico
+app.use(express.static(path.join(__dirname,'../public')))
+
+//rutas
+//http://localhost:4000/prueba
+app.get('/prueba', (req, res)=>{
+    res.send('esto es una prueba de una peticion get')
+})
+app.delete('/prueba', (req, res)=>{
+    res.send('aqui tendria que borrar un dato')
+})
