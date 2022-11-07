5425265974af9ac7ac5e50d9e460846dc9f061dc
Eliseo Espindola <eliseoespindola1@gmail.com>
2022-11-07 15:01:22 -0300

    agreagamos la conexion a la BD de produccion

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://127.0.0.1:27017/cafecito-c8i'
-
+//const url = 'mongodb://127.0.0.1:27017/cafecito-c8i'
+const url ='mongodb+srv://eliseoe95:38249520@cluster0.pqd832q.mongodb.net/cafecito-ee';
 mongoose.connect(url);
 
 const datosConexion = mongoose.connection;
